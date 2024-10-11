3115539748bbad5b6412306753108348ad9fbc53
Guillaume Renard <guillaume.renard@altusgroup.com>
2024-10-11 16:44:47 +0200

    move setup to test file

--- src/HelloWorld.test.tsx
+++ src/HelloWorld.test.tsx
@@ -1,8 +1,26 @@
-import { render } from "@testing-library/react";
+import { cleanup, render } from "@testing-library/react";
 import { expect, test } from "vitest";
 import HelloWorld from "./HelloWorld.js";
 import { worker } from "./mocks/browser.js";
 import { http, HttpResponse } from "msw";
+
+// from https://mswjs.io/docs/getting-started/integrate/node#setup
+// Establish API mocking before all tests.
+beforeAll(async () => {
+  await worker.start({ onUnhandledRequest: "error" });
+});
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => {
+  worker.resetHandlers();
+  cleanup();
+});
+
+// Clean up after the tests are finished.
+afterAll(() => {
+  worker.stop();
+});
 
 test("renders name", () => {
   const { getByText } = render(<HelloWorld name="Vitest" />);
